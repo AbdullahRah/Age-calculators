17d5253fee8b9bf79ec0c54c83f824ced717998b
Abdullah Rahman <55935285+AbdullahRah@users.noreply.github.com>
2022-10-27 09:02:40 -0600

    Add files via upload

---
+++ test.js
@@ -0,0 +1,16 @@
+//let myAge = 25;
+let myAge = parseFloat(prompt("Please enter age: "));
+if (myAge > 30){
+    alert ("You are older than 30!");
+    document.write("I feel old.. meh!!");
+} else if (myAge > 20){
+    alert ("You are older than 20!");
+    document.write("You're getting old!! wooooooowwwwwwww whats wrong with you??")
+} else if (myAge > 10){
+    alert ("You are older than 10!");
+    document.write("You are still young and have time to enjoy life!!")
+} else {
+    document.write("You are only human!")
+}
+
+//let myAge = parseFloat(prompt("Please enter age: "));
